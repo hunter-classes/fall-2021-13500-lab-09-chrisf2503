ee7ee1f05ff2e058bbebcfd18d6d5358be2596e1
chrisf2503 <christopher321flores@gmail.com>
2021-11-08 19:11:44 -0500

    Update funcs.cpp

--- funcs.cpp
+++ funcs.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "funcs.h"
 #include "coord3d.h"
+#include "math.h"
 // add functions here
 
 double length(Coord3D *p){
-    //double operator1 = pow(*x, 2) + 
-    return 0.0;
+    double firstOp = pow(p->x, 2) + pow(p->y, 2) + pow(p->z, 2);
+    double secondOp = sqrt(firstOp);
+    return secondOp;
 }
