a6776f15ee7fe651a3e592ffb9b91126e0f6cbc9
chrisf2503 <christopher.flores62@myhunter.cuny.edu>
2021-11-11 13:38:34 -0500

    update

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,16 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "funcs.h"
-
+#include "coord3d.h"
+#include "math.h"
 // add your tests here
+TEST_CASE("Task A: "){
+    Coord3D p = {10, 20, 30};
+    double answer = sqrt((10*10) + (20*20) + 30*30);
+    CHECK(length(&p) == answer);
+    Coord3D p1 = {2, 4, 9};
+    answer = sqrt((2*2) + (4*4) + (9*9));
+    //deleteCoord3D(Coord3D &p);
+    CHECK(length(&p1) == answer);
+    //CHECK();
+}
